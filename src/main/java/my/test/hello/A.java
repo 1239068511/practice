bf520df162b2abc4b0e5045ed09434e8ac88149f
1239068511 <1239068511@qq.com>
2020-04-09 16:20:51 +0800

    Update A.java

--- src/main/java/my/test/hello/A.java
+++ src/main/java/my/test/hello/A.java
@@ -15,6 +15,7 @@ public class A {
         System.out.println("这里是gitLab");
         System.out.println("这是");
         System.out.println("这是merger");
+        System.out.println("3");
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
